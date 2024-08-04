8ab2c969ffbb6ba686f06133bfb68714febfd9c9
Ashishverma001 <111993809+Ashishverma001@users.noreply.github.com>
2024-08-04 13:01:33 +0530

    do

--- script.js
+++ script.js
@@ -8,21 +8,30 @@ function togglemenu() {
 // Function to handle theme toggling
 function handleThemeToggle() {
     const themeToggleButton = document.getElementById('theme-toggle');
+    const nav = document.querySelector('nav');
+    const menuToggleButton = document.querySelector(".hamburger-icon");
     const currentTheme = localStorage.getItem('theme') || 'light';
     
     if (currentTheme === 'dark') {
         document.body.classList.add('dark-mode');
+        nav.classList.add('dark-mode');
+        themeToggleButton.classList.add('dark-mode');
+        menuToggleButton.classList.add('dark-mode');
         themeToggleButton.textContent = '☀️';
     }
 
     themeToggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        nav.classList.toggle('dark-mode');
+        themeToggleButton.classList.toggle('dark-mode');
+        menuToggleButton.classList.toggle('dark-mode');
         const isDarkMode = document.body.classList.contains('dark-mode');
         
         themeToggleButton.textContent = isDarkMode ? '☀️' : '🌙';
         localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
     });
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     handleThemeToggle();
